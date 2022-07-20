d4e15ccd53866cb061c7bc08a4a7ffccb6b5b199
Caio Ribeiro <caioribeirodev@gmail.com>
2022-07-20 11:27:21 -0300

    global styles

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,12 +16,15 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  
 
   background-color: ${props => props.theme.primary};
 
-  /* ${props => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `
-  }} */
-`
+  `
+  // ${props => {
+  //   return css`
+  //     background-color: ${buttonVariants[props.variant]};
+  // }} 
